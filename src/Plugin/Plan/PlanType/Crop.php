b9a081d638441f727d96ba141ef1ccb019596452
Michael Stenta <mike@mstenta.net>
2020-12-08 07:47:34 -0500

    Add Crop plan type.

---
+++ src/Plugin/Plan/PlanType/Crop.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_crop_plan\Plugin\Plan\PlanType;
+
+use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
+
+/**
+ * Provides the crop plan type.
+ *
+ * @PlanType(
+ *   id = "crop",
+ *   label = @Translation("Crop"),
+ * )
+ */
+class Crop extends FarmPlanType {
+
+}
