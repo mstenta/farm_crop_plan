5cd1c4f771a5ebaa6bb8427b1df38ef97e5e9e52
Michael Stenta <mike@mstenta.net>
2024-01-08 18:03:23 -0500

    Add a crop_planting plan record relationship type.

---
+++ src/Plugin/PlanRecord/PlanRecordType/CropPlanting.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_crop_plan\Plugin\PlanRecord\PlanRecordType;
+
+use Drupal\farm_entity\Plugin\PlanRecord\PlanRecordType\FarmPlanRecordType;
+
+/**
+ * Provides the crop planting plan record type.
+ *
+ * @PlanRecordType(
+ *   id = "crop_planting",
+ *   label = @Translation("Crop Planting"),
+ * )
+ */
+class CropPlanting extends FarmPlanRecordType {
+
+}
