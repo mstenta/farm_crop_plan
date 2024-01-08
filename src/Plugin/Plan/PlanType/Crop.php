2b208a27678ed87562210aac1b05960e4a4a2a1e
Michael Stenta <mike@mstenta.net>
2024-01-08 17:20:35 -0500

    Add Season term reference field.

--- src/Plugin/Plan/PlanType/Crop.php
+++ src/Plugin/Plan/PlanType/Crop.php
@@ -14,4 +14,30 @@ use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
  */
 class Crop extends FarmPlanType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'season' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Season'),
+        'description' => $this->t('Assign this to a season for easier searching later.'),
+        'target_type' => 'taxonomy_term',
+        'target_bundle' => 'season',
+        'auto_create' => TRUE,
+        'multiple' => TRUE,
+        'weight' => [
+          'form' => -50,
+          'view' => -50,
+        ],
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
 }
