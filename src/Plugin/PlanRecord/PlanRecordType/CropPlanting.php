bb2892dbdc0cea5f0afc8af0b34ba4b928adac37
Michael Stenta <mike@mstenta.net>
2024-01-08 18:07:24 -0500

    Add a single-value required Plant asset reference field.

--- src/Plugin/PlanRecord/PlanRecordType/CropPlanting.php
+++ src/Plugin/PlanRecord/PlanRecordType/CropPlanting.php
@@ -14,4 +14,26 @@ use Drupal\farm_entity\Plugin\PlanRecord\PlanRecordType\FarmPlanRecordType;
  */
 class CropPlanting extends FarmPlanRecordType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'plant' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Plant'),
+        'description' => $this->t('Associates the crop plan with a Plant asset.'),
+        'target_type' => 'asset',
+        'target_bundle' => 'plant',
+        'cardinality' => 1,
+        'required' => TRUE,
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
 }
