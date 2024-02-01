e5eb5d32fb924a34b3d838f2e542c1c66bdd2546
Paul Weidner <paul.weidner@gmail.com>
2024-01-31 17:04:50 -0800

    Add CropPlanting bundle class

---
+++ src/Bundle/CropPlanting.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\farm_crop_plan\Bundle;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\plan\Entity\PlanRecord;
+
+/**
+ * Bundle logic for Crop Planting.
+ */
+class CropPlanting extends PlanRecord {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function label() {
+
+    // Build label with the referenced plan and plant.
+    if ($plan = $this->getPlan()) {
+      if ($plant = $this->get('plant')->first()?->entity) {
+        return $this->t('Crop planting: %plant - %plan', ['%plant' => $plant->label(), '%plan' => $plan->label()]);
+      }
+
+      // Use the plan if no plant reference.
+      return $this->t('Crop Planting - %plan', ['@plan' => $plan->label()]);
+    }
+
+    // Fallback to default.
+    return parent::label();
+  }
+
+}
