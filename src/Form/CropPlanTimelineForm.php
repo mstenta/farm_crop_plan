9eeee059b795a816b352d1c40d288f60ce42c95d
Paul Weidner <paul.weidner@gmail.com>
2024-07-25 17:33:54 -0700

    Update to use farm_timeline render element.

--- src/Form/CropPlanTimelineForm.php
+++ src/Form/CropPlanTimelineForm.php
@@ -5,6 +5,7 @@ namespace Drupal\farm_crop_plan\Form;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Drupal\farm_crop_plan\CropPlanInterface;
 use Drupal\plan\Entity\PlanInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -73,10 +74,10 @@ class CropPlanTimelineForm extends FormBase {
 
     // Toggle the timeline view by plant type (default) or by location.
     $mode_options = [
-      'plant-type' => $this->t('Plant type'),
+      'plant_type' => $this->t('Plant type'),
       'location' => $this->t('Location'),
     ];
-    $mode_default = 'plant-type';
+    $mode_default = 'plant_type';
     $form['options'] = [
       '#type' => 'details',
       '#title' => $this->t('Options'),
@@ -107,13 +108,12 @@ class CropPlanTimelineForm extends FormBase {
     $display_mode = $form_state->getValue('mode', $mode_default);
 
     // Render the timeline gantt chart.
+    $row_url = Url::fromRoute("farm_crop_plan.timeline_by_$display_mode", ['plan' => $plan->id()]);
     $form['timeline']['gantt'] = [
-      '#type' => 'html_tag',
-      '#tag' => 'div',
+      '#type' => 'farm_timeline',
+      '#rows' => [$row_url->setAbsolute()->toString()],
       '#attributes' => [
-        'id' => 'timeline',
         'data-table-header' => $mode_options[$display_mode],
-        'data-timeline-url' => 'plan/' . $plan->id() . '/timeline/' . $display_mode,
         'data-timeline-instantiator' => 'farm_crop_plan',
       ],
       '#attached' => [
