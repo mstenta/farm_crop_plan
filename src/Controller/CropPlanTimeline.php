99b29207705a64bdfaf365c604097bf1362c9868
Paul Weidner <paul.weidner@gmail.com>
2024-09-13 13:55:03 -0700

    Do not provide a top-level label for log tasks

--- src/Controller/CropPlanTimeline.php
+++ src/Controller/CropPlanTimeline.php
@@ -155,7 +155,6 @@ class CropPlanTimeline extends ControllerBase {
             $status = $log->get('status')->value;
             return [
               'id' => $this->uuidService->generate(),
-              'label' => $log->label(),
               'edit_url' => $edit_url,
               'start' => $log->get('timestamp')->value,
               'end' => $log->get('timestamp')->value + 86400,
